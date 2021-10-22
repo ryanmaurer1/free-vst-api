6f049575b7822e590835b1df511e31c33a3d353b
Ryan Maurer <rmaurer@Ryans-Macbook-Air.local>
2021-10-22 11:46:41 -0400

    initial commit

---
+++ src/main/java/com/example/freevst/FreeVstApplication.java
@@ -0,0 +1,13 @@
+package com.example.freevst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeVstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeVstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/freevst/FreeVstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freevst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeVstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
